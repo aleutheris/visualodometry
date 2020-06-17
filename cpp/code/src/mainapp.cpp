a1fe094e0f9f1dab0867a48208a6296c5df699d2
ampLe <ampeleuterio@gmail.com>
2020-06-17 08:53:25 +0200

    Hardcoded JSON created and stringified to output

--- cpp/code/src/mainapp.cpp
+++ cpp/code/src/mainapp.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <json.hpp>
 
 #include "pixels_generator_cbridge.h"
 
+using json = nlohmann::json;
 
 int main(int argc, char *argv[])
 {
@@ -26,8 +28,34 @@ int main(int argc, char *argv[])
     printf("%d %d\n", array_cenas[0][i], array_cenas[1][i]);
   }*/
 
-
   std::cout << atoi(argv[1]) + atoi(argv[2]) << std::endl;
+
+
+  // instead, you could also write (which looks very similar to the JSON above)
+  json j;
+
+  // add a number that is stored as double (note the implicit conversion of j to an object)
+  j["pi"] = 3.141;
+
+  // add a Boolean that is stored as bool
+  j["happy"] = true;
+
+  // add a string that is stored as std::string
+  j["name"] = "Niels";
+
+  // add another null object by passing nullptr
+  j["nothing"] = nullptr;
+
+  // add an object inside the object
+  j["answer"]["everything"] = 42;
+
+  // add an array that is stored as std::vector (using an initializer list)
+  j["list"] = { 1, 0, 2 };
+
+  // add another object (using an initializer list of pairs)
+  j["object"] = { {"currency", "USD"}, {"value", 42.99} };
+
+  std::cout << j.dump() << std::endl;
 
   return 0;
 }
