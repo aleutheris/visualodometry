c5ac6b41e9288028b09a1904a3bb93533ec40110
ampLe <ampeleuterio@gmail.com>
2020-06-23 22:47:46 +0200

    Three D3 svg circles generated on html

--- web/public/script.js
+++ web/public/script.js
@@ -1,13 +1,39 @@
-(function(){
-var app = angular.module("githubViewer", [])
+(function() {
+	var app = angular.module("githubViewer", [])
 
-var MainController = function($scope)
-{
-  $scope.message = "Hello Angular";
-}
+	var MainController = function($scope) {
+		$scope.message = "Hello Angular";
+	}
 
-app.controller("MainController", MainController);
+	var CirclesController = function($scope) {
+		var spaceCircles = [30, 70, 110];
 
+		var svgContainer = d3.select("body").append("svg")
+			.attr("width", 200)
+			.attr("height", 200);
+
+		var circles = svgContainer.selectAll("circle")
+			.data(spaceCircles)
+			.enter()
+			.append("circle");
+
+		var circleAttributes = circles
+			.attr("cx", function(d) { return d; })
+			.attr("cy", function(d) { return d; })
+			.attr("r", 20)
+			.style("fill", function(d) {
+				var returnColor;
+				if (d === 30) {
+					returnColor = "green";
+				} else if (d === 70) {
+					returnColor = "purple";
+				} else if (d === 110) { returnColor = "red"; }
+				return returnColor;
+			});
+	}
+
+	app.controller("MainController", MainController);
+	app.controller("CirclesController", CirclesController);
 }());
 
 
@@ -45,14 +71,12 @@ const bongs = document.getElementById('bongs')
 var request2 = new XMLHttpRequest()
 request2.open('GET', 'http://localhost:5001/vodometry/us-central1/app/x=30&y=40&numberpixels=10', true)
 request2.onload = function() {
-  if (request2.status === 200) 
-  {
-    bongs.innerHTML = this.response;
-  } 
-  else 
-  {
-    bongs.innerHTML = 'An error occurred during your request: ' +  request2.status + ' ' + request2.statusText;
-  }
+	if (request2.status === 200) {
+		bongs.innerHTML = this.response;
+	}
+	else {
+		bongs.innerHTML = 'An error occurred during your request: ' + request2.status + ' ' + request2.statusText;
+	}
 }
 
 request2.send()
