2068860dcb3d7471340d0c66f1385b6f6c6fc34d
ampLe <ampeleuterio@gmail.com>
2020-06-24 08:17:51 +0200

    RESTful GET converted to angularjs

--- web/public/script.js
+++ web/public/script.js
@@ -1,8 +1,13 @@
 (function() {
 	var app = angular.module("githubViewer", [])
 
-	var MainController = function($scope) {
-		$scope.message = "Hello Angular";
+	var MainController = function($scope, $http) {
+		
+		var promise = $http.get("http://localhost:5001/vodometry/us-central1/app/x=30&y=40&numberpixels=10");
+		
+		promise.then(function(response){
+			$scope.message = response.data;
+		});
 	}
 
 	var CirclesController = function($scope) {
@@ -35,48 +40,3 @@
 	app.controller("MainController", MainController);
 	app.controller("CirclesController", CirclesController);
 }());
-
-
-
-
-/*
-function refresh(_this) {
-  // Disable the button and remove current BONGS text.
-  _this.disabled = true;
-  document.getElementById('bongs').innerHTML = '...';
-
-  // Prepare an ajax request to use the API to get the current BONGS from the API.
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (request.readyState === 4) {
-      // Re-enable the button.
-      _this.disabled = false;
-      var bongsContainer = document.getElementById('bongs');
-      if (request.status === 200) {
-        // Replace the BONG text with the response from the API.
-        bongsContainer.innerHTML = JSON.parse(request.responseText).bongs;
-      } else {
-        bongsContainer.innerHTML = 'An error occurred during your request: ' +  request.status + ' ' + request.statusText;
-      }
-    }
-  }
-  request.open('Get', '/api');
-
-  // Start the ajax request.
-  request.send();
-}*/
-
-const bongs = document.getElementById('bongs')
-
-var request2 = new XMLHttpRequest()
-request2.open('GET', 'http://localhost:5001/vodometry/us-central1/app/x=30&y=40&numberpixels=10', true)
-request2.onload = function() {
-	if (request2.status === 200) {
-		bongs.innerHTML = this.response;
-	}
-	else {
-		bongs.innerHTML = 'An error occurred during your request: ' + request2.status + ' ' + request2.statusText;
-	}
-}
-
-request2.send()
