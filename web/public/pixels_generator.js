79ac92e1b8b7d806a010f44193d21c544d3b9189
ampLe <ampeleuterio@gmail.com>
2020-07-22 09:18:31 +0200

    canvas is able to be dragged and zoomed

--- web/public/pixels_generator.js
+++ web/public/pixels_generator.js
@@ -6,101 +6,24 @@ var pixelsColor = "white";
 var tooltip = [];
 
 
-var createTooltip = function(div, imageData, numberOfPixels) {
-	for (var i = 0; i < numberOfPixels; i++) {
-		tooltip[i] = d3.select("#" + div)
-			.append("div")
-			.style("position", "absolute")
-			.style("visibility", "hidden")
-			.text("(" + imageData["pixels"][i].x + "," + imageData["pixels"][i].y + ")");
-	}
+function setPixel(ctx, x, y) {
+	ctx.beginPath();
+	ctx.fillStyle = "white";
+	ctx.rect(x, y, 1, 1);
+	ctx.fill();
 }
 
-var createPixelsGenerator = function(div, image) {
-	var dyncSvgPixelsGenerated = "svg" + div;
+var renderPixelsPicture = function(ctx, image, pixelsGenerated, numberOfPixels, relativeScale, positionX, positionY) {
+	ctx.clearRect(0, 0, image.x, image.y);
+	ctx.beginPath();
 
-	d3.select('#' + dyncSvgPixelsGenerated).remove();
-
-	svgContainer = d3.select("#" + div)
-		.append("svg")
-		.attr("id", dyncSvgPixelsGenerated)
-		.attr("width", image.x)
-		.attr("height", image.y)
-		.call(d3.zoom()
-			.scaleExtent([.5, 20])
-			.on("zoom", function() {
-				svgContainer.attr("transform", d3.event.transform)
-			}))
-		.append("g")
-}
-
-var renderPictureBackgroud = function(image) {
-	svgContainer
-		.append("rect")
-		.attr("id", "background")
-		.attr("x", 0)
-		.attr("y", 0)
-		.attr("width", image.x)
-		.attr("height", image.y)
-		.attr("fill", "grey");
-}
-
-var renderPixel = function(x, y, length, color, id) {
-	svgContainer
-		.append("rect")
-		.attr("id", "id" + id)
-		.attr("x", x)
-		.attr("y", y)
-		.attr("width", length)
-		.attr("height", length)
-		.attr("fill", color);
-
-
-}
-
-var renderPixels = function(imageData, numberOfPixels) {
-	for (var i = 0; i < numberOfPixels; i++) {
-		renderPixel(imageData["pixels"][i].x, imageData["pixels"][i].y, pixelSize, pixelsColor, i);
-		mouseHover(i);
-	}
-}
-
-var mouseHover = function(id) {
-	d3.select("#id" + id)
-		.on("mouseover", function() { return tooltip[id].style("visibility", "visible"); })
-		.on("mousemove", function() {
-			return tooltip[id].style("top", (event.pageY + 50) + "px").style("left", (event.pageX + 50) + "px");
-		})
-		.on("mouseout", function() { return tooltip[id].style("visibility", "hidden"); });
-}
-
-
-function setPixel(imageData, x, y, r, g, b, a) {
-	var index = (x + y * imageData.width) * 4;
-	imageData.data[index + 0] = r;
-	imageData.data[index + 1] = g;
-	imageData.data[index + 2] = b;
-	imageData.data[index + 3] = a;
-}
-
-var renderPixelsPicture = function(div, image, pixelsGenerated, numberOfPixels) {/*
-	createTooltip(div, imageData, numberOfPixels);
-	createPixelsGenerator(div, image);
-	renderPictureBackgroud(image);
-	renderPixels(imageData, numberOfPixels);*/
-
-
-	var canvas = document.getElementById("canvas1");
-	var ctx = canvas.getContext("2d");
+	ctx.scale(relativeScale, relativeScale);
 	ctx.fillStyle = "black";
-	ctx.fillRect(0, 0, image.x, image.y);
 
-	//var imageData = ctx.createImageData(image.x, image.x);
-	var imageData = ctx.getImageData(0, 0, image.x, image.y);
+	ctx.rect(positionX, positionY, image.x, image.y);
+	ctx.fill();
 
 	for (var i = 0; i < numberOfPixels; i++) {
-		setPixel(imageData, pixelsGenerated["pixels"][i].x, pixelsGenerated["pixels"][i].y, 255, 255, 255, 255);
+		setPixel(ctx, positionX + pixelsGenerated["pixels"][i].x, positionY + pixelsGenerated["pixels"][i].y);
 	}
-
-	ctx.putImageData(imageData, 0, 0);
 }
--- web/public/script.js
+++ web/public/script.js
@@ -21,6 +21,9 @@
 	var numberOfPixels = 200;
 	var divFramePixelsGenerated = "FramePixelsGenerated";
 	var divFramePixelsGeneratedAndTransformed = "FramePixelsGeneratedAndTransformed";
+	var canvas = document.getElementById("canvas1");
+	var ctx = canvas.getContext("2d");
+	var pixelsGenerated = {};
 
 
 	app.directive("framePixelsGenerated", function() {
@@ -41,8 +44,51 @@
 
 
 	var FrameController = function($scope, $http) {
+		$scope.scale = 1;
 		$scope.image = image;
 		$scope.numberOfPixels = numberOfPixels;
+		var mouseX = 0;
+		var mouseY = 0;
+		var mouseDownX = 0;
+		var mouseDownY = 0;
+		var imagePositionX = 0;
+		var imagePositionY = 0;
+		var isDraggable = false;
+		var previousScale = 1;
+		var canvasOffSetLeft = {};
+		var canvasOffSetTop = {};
+
+		$scope.mouseDown = function(event) {
+			canvasOffSetLeft = angular.element(event.target).prop('offsetLeft');
+			canvasOffSetTop = angular.element(event.target).prop('offsetTop');
+
+			mouseDownX = event.pageX - canvasOffSetLeft - imagePositionX;
+			mouseDownY = event.pageY - canvasOffSetTop - imagePositionY;
+
+			if (mouseX >= 0 && mouseX <= image.x && mouseY >= 0 && mouseY <= image.y) {
+				isDraggable = true;
+			}
+		};
+
+		$scope.mouseMove = function(event) {
+			if (isDraggable) {
+				mouseX = event.pageX - canvasOffSetLeft;
+				mouseY = event.pageY - canvasOffSetTop;
+
+				imagePositionX = mouseX - mouseDownX;
+				imagePositionY = mouseY - mouseDownY;
+
+				renderPixelsPicture(ctx, image, pixelsGenerated, numberOfPixels, $scope.scale / previousScale, imagePositionX, imagePositionY);
+			}
+		};
+
+		$scope.mouseUp = function() {
+			isDraggable = false;
+		};
+
+		$scope.mouseOut = function() {
+			isDraggable = false;
+		};
 
 		$scope.generate = function() {
 			image.x = $scope.image.x;
@@ -53,11 +99,18 @@
 			//var promise = $http.get("https://us-central1-vodometry.cloudfunctions.net/app/x=" + image.x + "&y=" + image.y + "&numberpixels=" + numberOfPixels);
 
 			promise.then(function(response) {
-				var pixelsGenerated = response.data;
-				renderPixelsPicture(divFramePixelsGenerated, image, pixelsGenerated, numberOfPixels);
-				//renderPixelsPicture(divFramePixelsGeneratedAndTransformed, image, imageData, numberOfPixels);
+				pixelsGenerated = response.data;
+				renderPixelsPicture(ctx, image, pixelsGenerated, numberOfPixels, $scope.scale / previousScale, 0, 0);
+				previousScale = $scope.scale;
 			});
 		};
+
+		$scope.$watch("scale", function(newValue, oldValue) {
+			if (newValue != oldValue) {
+				renderPixelsPicture(ctx, image, pixelsGenerated, numberOfPixels, $scope.scale / previousScale, imagePositionX, imagePositionY);
+				previousScale = $scope.scale;
+			}
+		});
 	}
 
 	app.controller("FrameController", ["$scope", "$http", FrameController]);
